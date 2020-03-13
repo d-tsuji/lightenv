990a532c27fba0c254f18522ef1263a6c26c2679
d-tsuji <dram.dt.shonan@gmail.com>
2020-03-13 22:39:12 +0900

    add example test

---
+++ example_test.go
@@ -0,0 +1,47 @@
+package lightenv_test
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/d-tsuji/lightenv"
+)
+
+func Example() {
+	os.Setenv("APP_URL", "http://localhost")
+	os.Setenv("PORT", "8888")
+	os.Setenv("CONCURRENCY_NUM", "100")
+
+	type Sample struct {
+		Url            string `name:"APP_URL" required:"true"`
+		PORT           string `required:"true"`
+		ConcurrencyNum int    `name:"CONCURRENCY_NUM" required:"true"`
+	}
+
+	var s Sample
+	if err := lightenv.Process(&s); err != nil {
+		fmt.Printf("%+v", err)
+	}
+
+	fmt.Printf("%+v", s)
+
+	// Output: {Url:http://localhost PORT:8888 ConcurrencyNum:100}
+}
+
+func ExampleProcess() {
+	os.Setenv("APP_URL", "http://localhost")
+	os.Setenv("PORT", "8888")
+
+	type Sample struct {
+		Url            string `name:"APP_URL" required:"true"`
+		PORT           string `required:"true"`
+		ConcurrencyNum int    `name:"CONCURRENCY_NUM" required:"true"`
+	}
+
+	var s Sample
+	if err := lightenv.Process(&s); err != nil {
+		fmt.Printf("%+v", err)
+	}
+
+	// Output: required key CONCURRENCY_NUM missing value
+}
